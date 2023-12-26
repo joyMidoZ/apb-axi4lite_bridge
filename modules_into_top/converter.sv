9a58e6cb15f53b49018c2d623ad0ca3d1939f438
Misha Kovalko <optixmik@gmail.com>
2023-12-26 08:10:24 +0300

    Feat(убрал защелки)

--- modules_into_top/converter.sv
+++ modules_into_top/converter.sv
@@ -41,14 +41,15 @@ module converter
           full_D_read,
 
     //TO fifo
-    output logic push_A, 
-                 pop_A, 
-                 push_D,pop_D, 
-                 push_D_read, 
-                 pop_D_read
+    output      logic  push_A, 
+                logic  pop_A, 
+                logic  push_D,
+                logic  pop_D, 
+                logic  push_D_read, 
+                logic  pop_D_read
 
 );
-    bit ping = 0;
+    
 
     assign awready = (~full_A)?1:0;
     assign arready = (~full_A)?1:0;
@@ -58,56 +59,149 @@ module converter
     assign bresp = (psel&penable&pslverr)?2'b10:2'b00;
     
     assign push_A = (~full_A&(awvalid|arvalid))?1:0;
-    assign push_D = (~full_D&wvalid)?1:0;
+    //assign pop_A = ((empty_A&wvalid&write)|(empty_A&~write&arvalid)) ? 1 : 0;
+    //assign pop_D = (empty_D&wvalid) ? 1 : 0;
+    assign push_D = (~full_D&wvalid) ? 1 : 0;
+    //assign push_D_read = (~write&pready&~full_D_read) ? 1 : 0;
+    assign pop_D_read = (empty_D_read&rvalid&rready) ? 1 : 0;
 
-    assign pop_D_read = (empty_D_read&rvalid&rready)?1:0;
-
-    logic selWait,preadyREG;
+    logic selWait,preadyREG,write;
     always_ff@(posedge clk or negedge rst)
     begin
+        //pop_A <= 0;
+        //pop_D <= 0;
         preadyREG <= pready;
+        if(awvalid) write <= 1;
+        else if(arvalid) write <= 0;
+        else write <= write;
         if(pready) selWait <= 1;
         else selWait <= 0;
     end
+/*
+    always_ff@(posedge clk or negedge rst)
+    begin
+        if(pready) psel <= 0;
+        else 
+        begin
+            if(write)
+                begin
+                    if(wvalid)
+                        begin
+                            psel <= 1;
+                            //pop_A <= 1;
+                            //pop_D <= 1;
+                            pwrite <= 1; 
+                        end
+                    else
+                        begin
+                            psel <= psel;
+                            //pop_A <= pop_A;
+                            //pop_D <= pop_D;
+                            pwrite <= pwrite;
+                        end
+                end
+            else
+                begin
+                    if(pready&~full_D_read)
+                        begin
+                            rvalid <= 1;
+                            psel <=0;
+                            if(~pslverr) begin 
+                                //push_D_read <= 1; 
+                                rresp <= 2'b00;
+                            end
+                            else begin 
+                                //push_D_read <= 0;
+                                rresp <= 2'b10;
+                            end
+                        end
+                    else begin 
+                        rvalid <= 0;
+                        psel <= 1;
+                        //pop_A <= 1;
+                        pwrite <= 0;
+                    end
+                    
+                        
+                    
+                end
+        end
+    end
+    */
     
     always_comb begin 
-        if(selWait)begin
-        psel = 0;
-        end
         pop_A = 0;
         pop_D = 0;
         rresp = 2'b00;
-        if(wvalid)begin
-            psel = 1;
-            pop_A = 1;
-            pop_D = 1;
-            pwrite = 1;
-        end
-        
-        if(~awvalid&arvalid) begin
-            psel = 1;
-            pop_A = 1;
-            pwrite = 0;
+        //psel = 0;
+        if(selWait)begin
+            psel = 0;
         end
 
-         if(~pwrite)
-                begin
-                    if(pready&~full_D_read)begin
-                        rvalid = 1;
-                        if(~pslverr) begin 
-                            push_D_read = 1; 
-                            
-                            rresp = 2'b00;
+        else
+            begin
+                if(write)
+                    begin
+                        if(wvalid)
+                            begin
+                                psel = 1;
+                                pop_A = 1;
+                                pop_D = 1;
+                                pwrite = 1;
+                            end
+
+                        else 
+                            begin
+                                psel = psel;
+                                pop_A = pop_A;
+                                pop_D = pop_D;
+                                pwrite = pwrite;
+                            end
+                    end
+                
+                else
+                    begin
+                        psel = 1;
+                        pop_A = 1;
+                        pwrite = 0;
+                        if(pready&~full_D_read)begin
+                            rvalid = 1;
+                            psel = 0;
+                            pop_A = 0;
+                            if(~pslverr) begin 
+                                push_D_read = 1; 
+                                rresp = 2'b00;
+                            end
+                            else begin 
+                                push_D_read = 0;
+                                rresp = 2'b10;
+                            end
                         end
                         else begin 
-                            push_D_read = 0;
+                            rvalid = 0;
+                            push_D_read = push_D_read;
+                            rresp = rresp;
+                        end 
                             
-                            rresp = 2'b10;
-                        end
                     end
-                    else rvalid = 0;
+                /*
+                if(~awvalid&arvalid) begin
+                    psel = 1;
+                    pop_A = 1;
+                    pwrite = 0;
                 end
+
+                else 
+                    begin
+                        psel = 0;
+                        pop_A = 0;
+                        pwrite = 0;
+                    end
+                */
+            end
+        
     end
+    
     assign pprot = (pwrite)? awprot:arprot;
     assign pstrb = (pwrite)? wstrb:pstrb;
 
